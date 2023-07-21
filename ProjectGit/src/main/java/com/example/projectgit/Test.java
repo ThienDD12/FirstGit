533cb3e9cc5adba482e6e41c1147b89f2a73a096
DDTHIEN\LAPTOP <doanducthien1999@gmail.com>
2023-07-21 16:43:50 +0700

    fix updateinfo

--- ProjectGit/src/main/java/com/example/projectgit/Test.java
+++ ProjectGit/src/main/java/com/example/projectgit/Test.java
@@ -3,6 +3,6 @@ package com.example.projectgit;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Thien CC");
+        System.out.println("Thien ");
     }
 }
